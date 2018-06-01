089617ac6ce2dcceeac1859691753f0d799dc3e0
Dario Coletto <dcoletto.sw@gmail.com>
2018-06-01 15:28:45 +0200

    Update RequestData.swift

--- Sources/MoyaSugar/RequestData.swift
+++ Sources/MoyaSugar/RequestData.swift
@@ -1,11 +1,14 @@
+import Alamofire
 import Moya
 
 public class RequestData {
     let body: Codable?
     let queryParams: [String: Any]
+    let queryParamsEncoding: Alamofire.ParameterEncoding
     
-    public init(body: Codable? = nil, queryParams: [String: Any] = [:]) {
+    public init(body: Codable? = nil, queryParams: [String: Any] = [:], queryParamsEncoding: Alamofire.ParameterEncoding = Alamofire.URLEncoding()) {
         self.body = body
         self.queryParams = queryParams
+        self.queryParamsEncoding = queryParamsEncoding
     }
 }
